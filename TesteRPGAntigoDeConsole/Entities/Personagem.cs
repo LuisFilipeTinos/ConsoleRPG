69842b9fc2fa8e95c2625cc2a2674bf8b03395fc
Luis Filipe Tinos <lftinos@gmail.com>
2021-01-29 14:00:42 -0300

    First commit

---
+++ TesteRPGAntigoDeConsole/Entities/Personagem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TesteRPGAntigoDeConsole.Entities
+{
+    class Personagem
+    {
+        public string Nome { get; private set; }
+
+        public string Classe { get; private set; }
+
+        public string Arma { get; private set; }
+
+
+
+        public Personagem(string nome, string classe, string arma)
+        {
+            Nome = nome;
+            Classe = classe;
+            Arma = arma;
+        }
+
+        
+
+
+    }
+}
---
+++ TesteRPGAntigoDeConsole/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TesteRPGAntigoDeConsole.Entities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------------RPG DOS GAROTOS MENINOS------------------------");
+            char resposta = 'n';
+
+            while (resposta == 'n')
+            {
+                Console.WriteLine("DIGITE O NOME DO SEU PERSONAGEM: ");
+                string nome = Console.ReadLine();
+                Console.WriteLine("DIGITE A CLASSE DO SEU PERSONAGEM: ");
+                string classe = Console.ReadLine();
+                Console.WriteLine("DIGITE SEU ARMAMENTO: ");
+                string arma = Console.ReadLine();
+                Personagem per = new Personagem(nome, classe, arma);
+
+                Console.WriteLine();
+
+                Console.WriteLine("OLÁ, JOGADOR! SEUS DADOS ESTÃO CORRETOS? (s/n)");
+                Console.WriteLine();
+                Console.WriteLine("NOME: " + per.Nome + " CLASSE: " + per.Classe + " ARMA: " + per.Arma);
+                resposta = char.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                if (resposta == 's')
+                {
+                    Console.WriteLine("VAMOS COMEÇAR A AVENTURA!");
+                }
+                else
+                {
+                    Console.WriteLine("INSIRA SEUS DADOS NOVAMENTE");
+                }
+            }
+            
+
+        }
+    }
+}
